648afc035a82c0f3b18e42be6dfaaded68e25694
Pål Hasle <Pl Hasle>
2017-08-19 20:58:39 +0200

    Add project files.

---
+++ AdventureWorksConsole/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AdventureWorksDAL;
+using Microsoft.SqlServer.Types;
+using System.Data.Entity.SqlServer;
+
+namespace AdventureWorksConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlServerTypes.Utilities.LoadNativeAssemblies(AppDomain.CurrentDomain.BaseDirectory);
+            SqlProviderServices.SqlServerTypesAssemblyName = "Microsoft.SqlServer.Types, Version=14.0.0.0, Culture=neutral, PublicKeyToken=89845dcd8080cc91";
+           
+            
+            using (var context = new AdventureWorks2014Entities())
+            {
+
+                var adr = context.Address.Take(100);
+                foreach (var address in adr)
+                {
+                    Console.WriteLine("address: " + address.AddressLine1+ "  --  city: " + address.City);
+                }
+            }
+
+        }
+    }
+}
---
+++ AdventureWorksDAL/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventureWorksDAL
+{
+    public class Class1
+    {
+        public void test()
+        {
+
+            using (var context = new AdventureWorks2014Entities())
+            {
+                foreach (var address in context.Address)
+                {
+                    Console.WriteLine(address.AddressID);
+                }
+            }
+        }
+    }
+}
